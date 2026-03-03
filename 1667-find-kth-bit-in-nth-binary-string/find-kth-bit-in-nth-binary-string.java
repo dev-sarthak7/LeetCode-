7725c1abb856779a6d3e194f8024cd54e38b1ae9
Sarthak Satish Borekar <sarthakborekarcr7s@gmail.com>
2026-03-03 23:49:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (94.95%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -1,30 +1,19 @@
 class Solution {
-    String invert(String s){
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='1'){
-                sb.append(0);
-            }else{
-                sb.append(1);
-            }
-        }
-        return sb.reverse().toString();
-        
-    }
     public char findKthBit(int n, int k) {
-        String[] bn= new String[n+1];
-        bn[0]="0";
-        for(int i=1;i<=n;i++){
-            StringBuilder sb= new StringBuilder();
-            sb.append(bn[i-1]);
-            sb.append(1);
-            sb.append(invert(bn[i-1]));
-            bn[i]=sb.toString();
-
-
+        int len= (1<<n) -1;
+        if (n==1){
+            return '0';
         }
-        String sn=bn[n];
+        int mid= len/2 +1;
 
-        return sn.charAt(k-1);
+        if(k< mid){
+            return findKthBit(n-1,k);
+        }else if(k==mid){
+            return '1';
+        }else{
+            char ch= findKthBit(n-1, (len-(k-1)));
+            return (ch=='1')? '0':'1';
+        }
+
     }
 }
