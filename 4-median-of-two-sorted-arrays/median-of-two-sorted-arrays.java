74647fa64d4885cc7c8ec416d39d329cd072285a
Sarthak Satish Borekar <sarthakborekarcr7s@gmail.com>
2026-07-21 12:59:06 +0530

    Time: 1 ms (100.00%) | Memory: 48.7 MB (72.30%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,35 +1,35 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n= nums1.length;
-        int m= nums2.length;
-        int i=0;
-        int j=0;
-        int[] sorted= new int[m+n];
-        int count=0;
-        while(i<n && j<m){
-            if(nums1[i]<=nums2[j]){
-                sorted[count]=nums1[i];
-                i++;
+        if(nums1.length>nums2.length){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        int m=nums1.length;
+        int n= nums2.length;
+       
+        int low=0;
+        int high=m;
+        while(low<=high){
+            int partitionA=low+ (high-low)/2;
+            int partitionB=(m+n+1)/2- partitionA;
+
+            int maxLeftA= (partitionA==0)? Integer.MIN_VALUE: nums1[partitionA-1];
+            int maxLeftB= (partitionB==0)? Integer.MIN_VALUE: nums2[partitionB-1];
+            int minRightA= (partitionA==m)?Integer.MAX_VALUE:nums1[partitionA];
+            int minRightB=(partitionB==n)?Integer.MAX_VALUE: nums2[partitionB];
+
+            if(maxLeftA<=minRightB && maxLeftB<= minRightA){
+                if((m+n)%2==0){
+                    return (Math.max(maxLeftA,maxLeftB)+Math.min(minRightA,minRightB))/2.0;
+                }else{
+                    return Math.max(maxLeftA,maxLeftB);
+                }
+            }else if(maxLeftA > minRightB){
+                high= partitionA-1;
             }else{
-                sorted[count]=nums2[j];
-                j++;
+                low=partitionA+1;
             }
-            count++;
         }
-        while(i<n){
-            sorted[count]=nums1[i];
-            i++;
-            count++;
-        }
-        while(j<m){
-            sorted[count]=nums2[j];
-            j++;
-            count++;
-        }
-        if((m+n)%2==0){
-            return (double) (sorted[(m+n)/2 ] +sorted[((m+n)/2) -1])/2;
-        }else{
-            return (double) sorted[((m+n)/2) ];
-        }
+        return 0;
+
     }
 }
