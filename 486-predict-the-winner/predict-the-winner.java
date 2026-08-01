b6b3fa907492124a1b123ca6b58710fcee8e75c8
Sarthak Satish Borekar <sarthakborekarcr7s@gmail.com>
2026-08-01 21:04:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (74.96%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,25 +1,34 @@
 class Solution {
-    int solve(int i, int j,int[] nums){
-        if(i>j){
+    int[][] dp = new int[21][21];
+    int solve(int i, int j, int[] nums) {
+        if (i > j) {
             return 0;
         }
-        if(i==j){
+        if (i == j) {
             return nums[i];
         }
-        int take_i= nums[i]+ Math.min(solve(i+2,j,nums),solve(i+1,j-1,nums));
-        int take_j= nums[j]+Math.min(solve(i+1,j-1,nums),solve(i,j-2,nums));
-        return Math.max(take_i,take_j);
-    }
-    public boolean predictTheWinner(int[] nums) {
-        int sum=0;
-        for(int x:nums){
-            sum+=x;
+        if (dp[i][j] != -1) {
+            return dp[i][j];
         }
-        int i=0;
-        int j=nums.length-1;
-        int player1= solve(i,j,nums);
-        int player2= sum-player1;
-        return player1>=player2;
+        int take_i = nums[i] + Math.min(solve(i + 2, j, nums), solve(i + 1, j - 1, nums));
+        int take_j = nums[j] + Math.min(solve(i + 1, j - 1, nums), solve(i, j - 2, nums));
+        return dp[i][j] = Math.max(take_i, take_j);
+    }
+
+    public boolean predictTheWinner(int[] nums) {
+        
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
+        }
+        int sum = 0;
+        for (int x : nums) {
+            sum += x;
+        }
+        int i = 0;
+        int j = nums.length - 1;
+        int player1 = solve(i, j, nums);
+        int player2 = sum - player1;
+        return player1 >= player2;
 
     }
 }
