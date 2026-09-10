f278cc064526e1523600d5b459cecc4b80e0cc42
Sarthak Satish Borekar <sarthakborekarcr7s@gmail.com>
2026-09-10 10:26:43 +0530

    Time: 1 ms (99.93%) | Memory: 45.3 MB (75.23%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,29 +1,21 @@
 class Solution {
-    int [][] dp;
     int n;
-    boolean [][] visited;
-    int solve(int i, int j,List<List<Integer>> list){
-        if(i>=n){
-            return 0;
+    int[][] dp;
+    int solve(int i, int j, List<List<Integer>> list){
+        if(i==n-1){
+            return list.get(i).get(j);
         }
-        
-        if(visited[i][j]){
+       
+        if(dp[i][j]!=Integer.MAX_VALUE){
             return dp[i][j];
         }
-        visited[i][j]=true;
-        int a= solve(i+1,j,list);
-        int b= solve(i+1,j+1,list);
-        return dp[i][j]=Math.min(a,b) + list.get(i).get(j);
+        return dp[i][j]=Math.min(solve(i+1,j,list),solve(i+1,j+1,list))+ list.get(i).get(j);
     }
     public int minimumTotal(List<List<Integer>> triangle) {
-        n=triangle.size();
-        dp= new int[n+1][n+1];
-        visited= new boolean[n+1][n+1];
-        for(boolean[] arr:visited){
-            Arrays.fill(arr,false);
-        }
-        for(int [] arr: dp){
-            Arrays.fill(arr,-1);
+        n= triangle.size();
+        dp=new int[n][n];
+        for(int [] arr:dp){
+            Arrays.fill(arr,Integer.MAX_VALUE);
         }
         return solve(0,0,triangle);
     }
