4d0de894cddd02a425c2e39636dd3803e0093413
Sarthak Satish Borekar <sarthakborekarcr7s@gmail.com>
2026-09-10 09:54:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (80.89%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,25 +1,33 @@
 class Solution {
-    int [][]dp;
     int m;
     int n;
-    int solve(int i,int j,int[][]grid){
-        if(i>=m || j>=n) return 0;
-        if(grid[i][j]==1) return 0;
-        if(i==m-1 && j==n-1){
+    int[][] dp;
+    int solve(int i, int j, int[][] arr){
+        if(i==m-1 && j==n-1 && arr[m-1][n-1]!=1){
             return 1;
+        }
+        if(i>=m || j>=n){
+            return 0;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        
-        
-        return dp[i][j]=solve(i,j+1,grid) + solve(i+1,j,grid);
+        if(arr[i][j]==1){
+            return 0;
+        }
+        if(i==m-1){
+            return dp[i][j]=solve(i,j+1,arr);
+        }
+        if(j==n-1){
+            return dp[i][j]=solve(i+1,j,arr);
+        }
+        return dp[i][j]=solve(i+1,j,arr) + solve(i,j+1,arr);
     }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        m=obstacleGrid.length;
-        n=obstacleGrid[0].length;
-        dp= new int[m+1][n+1];
-        for(int []arr:dp){
+        m= obstacleGrid.length;
+        n= obstacleGrid[0].length;
+        dp= new int[m][n];
+        for(int[] arr:dp){
             Arrays.fill(arr,-1);
         }
         return solve(0,0,obstacleGrid);
